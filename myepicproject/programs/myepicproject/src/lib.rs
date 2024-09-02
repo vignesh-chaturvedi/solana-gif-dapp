195e4acd5072e27487166b9aa0ce2a64f9dde330
Vignesh Chaturvedi <vigneshchaturvedi@gmail.com>
2024-09-02 18:58:07 +0530

    DApp --v1.5

---
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -0,0 +1,43 @@
+use anchor_lang::prelude::*;
+
+declare_id!("BrqsNiwWpiE8qUK6dSJtErk56C9E6tCdgaijGkJSbPLf");
+
+#[program]
+pub mod myepicproject {
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+  
+	// Another function woo!
+  pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+    // Get a reference to the account and increment total_gifs.
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs += 1;
+    Ok(())
+  }
+}
+
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+  #[account(init, payer = user, space = 9000)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+  pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
